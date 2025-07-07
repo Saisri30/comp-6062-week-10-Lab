cd219b6b82c47fb2340c1501539690e85a726b0f
Saisri30 <saisrijakkana@gmail.com>
2025-07-07 19:18:06 -0400

    Added files

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: 'Week 10 Lab',
+            firstName:'',
+            lastName:'',
+            quantity:0,
+            pricePerItem: 10
+        }
+    },
+    computed: {
+        fullName() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        totalPrice() {
+            return this.quantity * this.pricePerItem;
+        }
+    },
+    methods: {
+        clearFields() {
+            this.firstName= '';
+            this.lastName = '';
+            this.quantity = 0;
+            this.pricePerItem =10
+        }
+    }
+}
+);
+app.mount('#app');
